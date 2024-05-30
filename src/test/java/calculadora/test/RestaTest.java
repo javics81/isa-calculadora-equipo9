1fa97b21c136cc434a6e132eb3a2252f65b1d362
Javi <javier.camino@gmail.com>
2024-05-31 00:19:20 +0200

    Test de la suma y de la resta

--- src/test/java/calculadora/test/RestaTest.java
+++ src/test/java/calculadora/test/RestaTest.java
@@ -12,11 +12,59 @@ public class RestaTest {
     private final Operacion operacion = new Resta();
 
     @Test
-    public void test1() {
+    public void restaEnterosPositivosTest() {
+        double resultado = this.operacion.calcular(7, 4);
 
-        assertEquals(0,operacion.calcular());
+        assertEquals(3,resultado);
     }
 
+    @Test
+    public void restaEnterosNegativosTest() {
+        double resultado = this.operacion.calcular(-5, -4);
 
+        assertEquals(-1,resultado);
+    }
+
+    @Test
+    public void restaEnterosPositivoNegativoTest() {
+        double resultado = this.operacion.calcular(2, -9);
+
+        assertEquals(11,resultado);
+    }
+
+    @Test
+    public void restaEnterosNegativoPositivoTest() {
+        double resultado = this.operacion.calcular(-3, 5);
+
+        assertEquals(-8,resultado);
+    }
+
+    @Test
+    public void restaDecimalesPositivosTest() {
+        double resultado = this.operacion.calcular(3.2, 6.5);
+
+        assertEquals(-3.3,resultado);
+    }
+
+    @Test
+    public void restaDecimalesNegativosTest() {
+        double resultado = this.operacion.calcular(-7.4, -3.2);
+
+        assertEquals(-4.2,resultado);
+    }
+
+    @Test
+    public void restaDecimalesPositivoNegativoTest() {
+        double resultado = this.operacion.calcular(6.9, -1.7);
+
+        assertEquals(8.6,resultado);
+    }
+
+    @Test
+    public void restaDecimalesNegativoPositivoTest() {
+        double resultado = this.operacion.calcular(-4.7, 6.2);
+
+        assertEquals(-10.9,resultado);
+    }
 
 }
--- src/test/java/calculadora/test/SumaTest.java
+++ src/test/java/calculadora/test/SumaTest.java
@@ -1,9 +1,10 @@
 package calculadora.test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import calculadora.operaciones.Operacion;
 import calculadora.operaciones.Suma;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class SumaTest {
@@ -11,9 +12,58 @@ public class SumaTest {
     private final Operacion operacion = new Suma();
 
     @Test
-    public void test1() {
+    public void sumaEnterosPositivosTest() {
+        double resultado = this.operacion.calcular(7, 4);
 
-        assertEquals(0,operacion.calcular());
+        assertEquals(11,resultado);
     }
 
+    @Test
+    public void sumaEnterosNegativosTest() {
+        double resultado = this.operacion.calcular(-5, -4);
+
+        assertEquals(-9,resultado);
+    }
+
+    @Test
+    public void sumaEnterosPositivoNegativoTest() {
+        double resultado = this.operacion.calcular(2, -9);
+
+        assertEquals(-7,resultado);
+    }
+
+    @Test
+    public void sumaEnterosNegativoPositivoTest() {
+        double resultado = this.operacion.calcular(-3, 5);
+
+        assertEquals(2,resultado);
+    }
+
+    @Test
+    public void sumaDecimalesPositivosTest() {
+        double resultado = this.operacion.calcular(3.2, 6.5);
+
+        assertEquals(9.7,resultado);
+    }
+
+    @Test
+    public void sumaDecimalesNegativosTest() {
+        double resultado = this.operacion.calcular(-7.4, -3.1);
+
+        assertEquals(-10.5,resultado);
+    }
+
+    @Test
+    public void sumaDecimalesPositivoNegativoTest() {
+        double resultado = this.operacion.calcular(6.9, -1.7);
+
+        assertEquals(5.2,resultado);
+    }
+
+    @Test
+    public void sumaDecimalesNegativoPositivoTest() {
+        double resultado = this.operacion.calcular(-4.7, 6.2);
+
+        assertEquals(1.5,resultado);
+    }
 }
