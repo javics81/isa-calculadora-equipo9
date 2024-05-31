72484af9af1ea016a7c3f964eadc26856a65eb5f
julian <bahamontesjulian@gmail.com>
2024-05-31 16:32:36 +0200

    Test raiz cuadrada

--- src/test/java/calculadora/test/RaizCuadradaTest.java
+++ src/test/java/calculadora/test/RaizCuadradaTest.java
@@ -2,9 +2,11 @@ package calculadora.test;
 
 import calculadora.operaciones.Operacion;
 import calculadora.operaciones.RaizCuadrada;
+import calculadora.operaciones.UtilidadesOperaciones;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class RaizCuadradaTest {
@@ -12,11 +14,23 @@ public class RaizCuadradaTest {
     private final Operacion operacion = new RaizCuadrada();
 
     @Test
-    public void test1() {
-
-        assertEquals(0,operacion.calcular());
+    public void RaizCuadradaDecimalesTest() {
+        double resultado = this.operacion.calcular(78.35, 0);
+        assertTrue(UtilidadesOperaciones.absoluto(resultado - 8.852) < 0.001 );
     }
 
+    @Test
+    public void RaizCuadradaEnterosTest() {
+        double resultado = this.operacion.calcular(48, 0);
+        assertTrue(UtilidadesOperaciones.absoluto(resultado - 6.928) < 0.001 );
+    }
+
+
+    @Test
+    public void RaizCuadradaCeroTest() {
+        double resultado = this.operacion.calcular(0, 0);
+        assertEquals(0, resultado);
+    }
 
 
 }
