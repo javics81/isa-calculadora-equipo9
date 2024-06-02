02ad3d4a938dd4a3792e16c441232549c11cff64
julian <bahamontesjulian@gmail.com>
2024-06-02 11:12:10 +0200

    Clase exponencial

--- src/main/java/calculadora/operaciones/Division.java
+++ src/main/java/calculadora/operaciones/Division.java
@@ -1,11 +1,17 @@
 package calculadora.operaciones;
 
-public class Division implements Operacion{
+public class Division implements Operacion {
 
     @Override
     public double calcular(double... parametros) {
 
+        double primerTermino = parametros[0];
+        double segundoTermino = parametros[1];
 
-        return 0.0;
+        if(segundoTermino == 0) {
+            throw new IllegalArgumentException("No se puede dividir por cero");
+        }
+
+        return primerTermino/segundoTermino;
     }
 }
