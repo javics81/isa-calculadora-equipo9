569bc10bec169769a1afaf839aa9d82cbccb1b3e
julian <bahamontesjulian@gmail.com>
2024-06-02 11:17:34 +0200

    Implementación operación raiz cuadrada

--- src/main/java/calculadora/operaciones/RaizCuadrada.java
+++ src/main/java/calculadora/operaciones/RaizCuadrada.java
@@ -1,11 +1,41 @@
 package calculadora.operaciones;
 
-public class RaizCuadrada implements Operacion{
+public class RaizCuadrada implements Operacion {
+
+
 
     @Override
-    public double calcular(double... parametros) {
+    public double calcular(double... params) {
+        // Verificar que el número sea positivo
 
+        double numero = params[0];
 
-        return 0.0;
+        if (numero < 0) {
+            throw new IllegalArgumentException("El número debe ser positivo");
+        }
+        // Caso especial para la raíz cuadrada de 0
+        if (numero == 0) {
+            return 0;
+        }
+
+        // Inicialización de variables
+        double aproximacion = numero;
+        double mejorAproximacion;
+
+        while (true) {
+            mejorAproximacion = (aproximacion + numero / aproximacion) / 2;
+
+            // Verificar la precisión
+            if (UtilidadesOperaciones.absoluto(mejorAproximacion - aproximacion) < UtilidadesOperaciones.PRECISION) {
+                break;
+            }
+
+            aproximacion = mejorAproximacion;
+        }
+
+        return mejorAproximacion;
     }
+
+
 }
+
